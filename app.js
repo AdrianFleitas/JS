aefccb7eb35b599b1af781aaf6918739fd7590fa
Adrian Tobit Fleitas <adriantekelo@hotmail.com>
2017-04-10 18:56:12 -0400

    segundo proyecto

---
+++ app.js
@@ -0,0 +1,33 @@
+var i = 10;
+var nombre = 'Adrian';
+
+var f = function(){
+	alert('saludar');
+}
+
+
+var app = {
+	contar: function(){
+		for (var i = 0; i < 100; i++){
+			alert('contador'+i);
+		};
+	},
+	par:function(num){
+		if(num%2==0){
+			return true;
+		}else{
+		     return false;
+		}
+	},
+	ciclo:function(){
+	     for(var i=1; i<=10;i++){
+	     	if(this.par(i)){
+	     		alert(i+'es par');
+	     	}
+	     	else{
+	     		alert(i+'es impar');
+	     	}
+	     }
+		}
+	}
+app.ciclo();
